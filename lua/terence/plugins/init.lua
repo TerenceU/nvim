c402c7d5398dac57823fe95761b29d702a7c5352
Terence Usai <terence.usa@hotmail.com>
2023-08-24 21:57:37 +0200

    First plugins config

--- lua/terence/plugins/init.lua
+++ lua/terence/plugins/init.lua
@@ -1,13 +1,10 @@
 -- requires for single packages
-require('terence.plugins.telescope')
-
 return require('packer').startup(function(use)
     -- Plenary 
     use "nvim-lua/plenary.nvim"
 
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
-
     -- Fuzy Telescope
     use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }    
 
